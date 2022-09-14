f7c3fbf91a6538d3063cd95c261e1eb5db01064e
ledungdee <feelgoood98@gmail.com>
2022-09-15 04:18:40 +0700

    advanced login: remember checkbox, cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user =User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     # Log the user in and redirect to the user's show page.
     reset_session
+    remember(user)
+    params[:session][:remember_me] =='1' ? remember(user) : forget(user)
     log_in user
     redirect_to user 
     else
@@ -16,7 +18,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 
