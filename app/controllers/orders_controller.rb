e87c4b63369237e38754b9e641ea3bae3b30dfbc
ledungdee <feelgoood98@gmail.com>
2022-09-30 20:55:01 +0700

    fix bug when product image is blank

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
     def index
         @orders = current_user.orders
     end
+    def show
+        @order = Order.find_by(id:params[:id])
+        @order_items = @order.order_items
+    end
 end
